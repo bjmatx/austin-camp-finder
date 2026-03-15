df024edea2c24dcddfe5529270e7a28c4763baa3
bjmatx <bjmatx@gmail.com>
2026-03-15 18:17:56 -0500

    Update camps.js

--- netlify/functions/camps.js
+++ netlify/functions/camps.js
@@ -1,23 +1,31 @@
+const https = require('https');
+
 exports.handler = async function(event, context) {
   const TOKEN = process.env.AIRTABLE_TOKEN;
   const BASE_ID = 'app2crPcNCqz50HED';
   const TABLE_ID = 'tblEFmCzTTFXMuN50';
 
-  let allRecords = [];
-  let offset = null;
+  function httpsGet(url, token) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        headers: { 'Authorization': `Bearer ${token}` }
+      };
+      https.get(url, options, (res) => {
+        let data = '';
+        res.on('data', chunk => data += chunk);
+        res.on('end', () => resolve(JSON.parse(data)));
+      }).on('error', reject);
+    });
+  }
 
   try {
+    let allRecords = [];
+    let offset = null;
+
     do {
       let url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_ID}?pageSize=100`;
       if (offset) url += `&offset=${offset}`;
-
-      const res = await fetch(url, {
-        headers: { 'Authorization': `Bearer ${TOKEN}` }
-      });
-
-      if (!res.ok) throw new Error('Airtable fetch failed');
-
-      const data = await res.json();
+      const data = await httpsGet(url, TOKEN);
       allRecords = allRecords.concat(data.records);
       offset = data.offset;
     } while (offset);
